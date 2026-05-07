6ca57a8c2adab3fe26cc4611e7526bf88096da41
Justin <54604897+Just-In-N-Out@users.noreply.github.com>
2026-05-07 19:40:00 +0300

    Add: python-list-looping - submission-3

---
+++ Python For Beginners/python-list-looping/submission-3.py
@@ -0,0 +1,15 @@
+from typing import List # used to add type hint for List
+
+def count_x(nums: List[int], x: int) -> int:
+    z = 0
+    for elements in nums:
+        if x in nums:
+            z +=1
+            return z
+
+
+
+# do not modify below this line
+print(count_x([1, 2, 5, 6, 5], 5))
+print(count_x([4, 3, 6, 1, 6], 5))
+print(count_x([4, 7, 7, 6, 7, 6], 7))
