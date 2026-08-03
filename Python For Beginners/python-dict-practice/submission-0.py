3638885124ec9b0feb5caf450000cdd0f04f60d9
Justin <54604897+Just-In-N-Out@users.noreply.github.com>
2026-08-03 12:54:50 +0300

    Add: python-dict-practice - submission-0

---
+++ Python For Beginners/python-dict-practice/submission-0.py
@@ -0,0 +1,14 @@
+from typing import Dict # this adds type hinting for Dict
+
+def count_characters(word: str) -> Dict[str, int]:
+    for char in word:
+      print(char)
+
+
+
+
+# don't modify below this line
+print(count_characters("hello"))
+print(count_characters("world"))
+print(count_characters("hello world"))
+print(count_characters("this is a longer sentence"))
