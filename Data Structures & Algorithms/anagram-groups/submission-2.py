fbd7a4e399609a6e1d023a3d90e2b88aed307670
Justin <54604897+Just-In-N-Out@users.noreply.github.com>
2026-08-05 13:16:06 +0300

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict1={}
+        for x in strs: 
+            value = "".join.sort()
+            if value in dict1:
+                dict1[value] = dict1.append(x)
+            else:
+                dict1[value] = dict1[x]
+        return list(dict1.values())
